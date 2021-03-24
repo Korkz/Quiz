16cd70a455fc577f9d7ab2f49ba46325000e6a55
Korkz <77691313+Korkz@users.noreply.github.com>
2021-03-24 02:14:40 -0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -10,4 +10,69 @@ $(document).ready(function() {
 	$("#qOne").click(function() {
 		$("#ansOne").toggle(1000);
 	});
+	
+	
+	$("#revTwo").click(function() {
+		$("#revTwo").toggle(1000);
+		$("#flagTwo").toggle(1000);
+	});
+	$("#flagTwo").click(function() {
+		$("#revTwo").toggle(1000);
+		$("#flagTwo").toggle(1000);	
+	});
+	$("#qTwo").click(function() {
+		$("#ansTwo").toggle(1000);
+	});
+	
+	
+	$("#revThr").click(function() {
+		$("#revThr").toggle(1000);
+		$("#flagThr").toggle(1000);
+	});
+	$("#flagThr").click(function() {
+		$("#revThr").toggle(1000);
+		$("#flagThr").toggle(1000);	
+	});
+	$("#qThr").click(function() {
+		$("#ansThr").toggle(1000);
+	});
+	
+	
+	$("#revFour").click(function() {
+		$("#revFour").toggle(1000);
+		$("#flagFour").toggle(1000);
+	});
+	$("#flagFour").click(function() {
+		$("#revFour").toggle(1000);
+		$("#flagFour").toggle(1000);	
+	});
+	$("#qFour").click(function() {
+		$("#ansFour").toggle(1000);
+	});
+	
+	
+	$("#revFive").click(function() {
+		$("#revFive").toggle(1000);
+		$("#flagFive").toggle(1000);
+	});
+	$("#flagFive").click(function() {
+		$("#revFive").toggle(1000);
+		$("#flagFive").toggle(1000);	
+	});
+	$("#qFive").click(function() {
+		$("#anFive").toggle(1000);
+	});
+	
+	
+	$("#revSix").click(function() {
+		$("#revSix").toggle(1000);
+		$("#flagSix").toggle(1000);
+	});
+	$("#flagSix").click(function() {
+		$("#revSix").toggle(1000);
+		$("#flagSix").toggle(1000);	
+	});
+	$("#qSix").click(function() {
+		$("#ansSix").toggle(1000);
+	});	
 });
